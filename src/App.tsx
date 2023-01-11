43c7755e06b90fddc142fbeb83ad2a61cba11612
Andres Fernandez <jandresfgz@gmail.com>
2023-01-11 17:46:40 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Sum Pairs</h1>
+      <div>
+        Number list:
+        <input type="text" name="" id="" />
+      </div>
+      <div>
+        Sum to find:
+        <input type="text" name="" id="" />
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
